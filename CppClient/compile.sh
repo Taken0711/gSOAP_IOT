3144dad84f201cd2c8b8506ae5737c4c1730d938
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-04-12 11:07:10 +0200

    add client

--- CppClient/compile.sh
+++ CppClient/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#wsdl2h -o gen/MathsLibrary.h res/MathsLibrary.wsdl
-#cd gen
-#soapcpp2 MathsLibrary.h
-#cd ..
+wsdl2h -o gen/calc.h http://www.genivia.com/calc.wsdl
+cd gen
+soapcpp2 -j -CL -I/path/to/gsoap/import calc.h
+cd ..
 
-g++ -o test src/Main.cpp gen/soapH.h
+g++ -std=c++11 -o bin/calcclient src/Main.cpp gen/soapC.cpp gen/soapcalcProxy.cpp lib/gsoap/stdsoap2.cpp -I$(pwd)/lib/gsoap
--- CppClient/src/Main.cpp
+++ CppClient/src/Main.cpp
@@ -1,7 +1,13 @@
-#include "../gen/soapH.h"  // include the generated source code headers
-#include <iostream>
+#include "../gen/calc.nsmap"      // XML namespace mapping table (only needed once at the global level)
+#include "../gen/soapcalcProxy.h" // the proxy class, also #includes "soapH.h" and "soapStub.h"
 
-int main() {
-    std::cout << "Hello world !" << std::endl;
-    return 0;
+int main()
+{
+  calcProxy calc;
+  double sum;
+  if (calc.add(1.23, 4.56, sum) == SOAP_OK)
+    std::cout << "Sum = " << sum << std::endl;
+  else
+    calc.soap_stream_fault(std::cerr);
+  calc.destroy(); // same as: soap_destroy(calc.soap); soap_end(calc.soap);
 }
