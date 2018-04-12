92a3cfab8a65b11ccc4eab1265f32b6edf2d82df
Francois MELKONIAN <francois.melkonian@etu.unice.fr>
2018-04-12 09:02:43 +0200

    SOAP MathLibrary

---
+++ MathsLibrary/MathsLibrary/CustomFaultDetails.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace MathsLibrary
+{
+    public class CustomFaultDetails
+    {
+        [DataMember]
+        public string ErrorID { get; set; }
+        public string ErrorDetails { get; set; }
+    }
+}
---
+++ MathsLibrary/MathsLibrary/MathsOperations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+namespace MathsLibrary
+{
+    public class MathsOperations : IMathsOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                CustomFaultDetails ex = new CustomFaultDetails();
+                ex.ErrorID = "12345";
+                ex.ErrorDetails = "Specific error details here.";
+                throw new FaultException<CustomFaultDetails>(ex, "Reason: Testing");
+            }
+            return num1 / num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+    }
+}
